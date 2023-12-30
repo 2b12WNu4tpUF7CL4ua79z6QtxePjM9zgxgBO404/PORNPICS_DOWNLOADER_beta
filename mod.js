8e21005df3dcad5fbd616e5cb37ffb198a1d61e2
PhantomByte666 <137177490+2b12WNu4tpUF7CL4ua79z6QtxePjM9zgxgBO404@users.noreply.github.com>
2023-12-31 02:00:15 +1030

    Update mod.js
    added so you can give main url instead of album url etc
    
    for example
    
    https://www.pornpics.com/pornstars/danny-d/

--- mod.js
+++ mod.js
@@ -8,35 +8,55 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
-rl.question('Enter the URL: ', (url) => {
-  rl.close();
-
-  axios.get(url).then(response => {
+function getAlbumUrls(mainUrl) {
+  return axios.get(mainUrl).then(response => {
     const html = response.data;
     const $ = cheerio.load(html);
-    const imgElements = $("li.thumbwook a");
+    const linkElements = $("a.rel-link");
 
-    const imageUrls = new Set();
+    const urls = [];
+    linkElements.each((index, element) => {
+      const url = $(element).attr('href');
+      urls.push(url);
+    });
 
-    imgElements.each((index, element) => {
-      const imgUrl = $(element).attr('href');
-      const imgName = imgUrl.split('/').pop();
+    return urls;
+  });
+}
 
-      if (!imageUrls.has(imgUrl)) {
-        imageUrls.add(imgUrl);
+rl.question('Enter the URL: ', (mainUrl) => {
+  rl.close();
 
-        axios({
-          url: imgUrl,
-          responseType: 'stream',
-        }).then(response =>
-          new Promise((resolve, reject) => {
-            response.data
-              .pipe(fs.createWriteStream(`./images/${imgName}`))
-              .on('finish', () => resolve())
-              .on('error', e => reject(e));
-          })
-        );
-      }
+  getAlbumUrls(mainUrl).then(urls => {
+    urls.forEach(url => {
+      axios.get(url).then(response => {
+        const html = response.data;
+        const $ = cheerio.load(html);
+        const imgElements = $("li.thumbwook a");
+
+        const imageUrls = new Set();
+
+        imgElements.each((index, element) => {
+          const imgUrl = $(element).attr('href');
+          const imgName = imgUrl.split('/').pop();
+
+          if (!imageUrls.has(imgUrl)) {
+            imageUrls.add(imgUrl);
+
+            axios({
+              url: imgUrl,
+              responseType: 'stream',
+            }).then(response =>
+              new Promise((resolve, reject) => {
+                response.data
+                  .pipe(fs.createWriteStream(`./images/${imgName}`))
+                  .on('finish', () => resolve())
+                  .on('error', e => reject(e));
+              })
+            );
+          }
+        });
+      });
     });
   });
 });
