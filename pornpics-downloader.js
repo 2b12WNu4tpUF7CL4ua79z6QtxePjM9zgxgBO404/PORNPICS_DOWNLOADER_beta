bd5039ff4a8cab1da17a6eceb2adf5722abcfcf2
PhantomByte666 <137177490+2b12WNu4tpUF7CL4ua79z6QtxePjM9zgxgBO404@users.noreply.github.com>
2024-08-01 15:22:33 +0930

    Update and rename mod.js to pornpics-downloader.js
    added standard message

---
+++ pornpics-downloader.js
@@ -0,0 +1,3 @@
+# i have finished building a more refine script and renamed the file name
+# i am currently just finishing off some final touches in visual studio code
+# i will update the code once done but for now i have removed the old code
